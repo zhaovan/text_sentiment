84b0a7d93c81571867eb6a1d9933a4ae196a86fc
sonny3690 <sonny3690@gmail.com>
2018-12-01 09:52:59 -0500

    including greasymonkey stuff

--- extension/background.js
+++ extension/background.js
@@ -1,27 +1,33 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+// chrome.browserAction.onClicked.addListener(function(tab) {
 
-	// reloads stuff
-	localStorage.tabToReload = tab.id;
-    chrome.runtime.reload();
+// 	// reloads stuff
+// 	localStorage.tabToReload = tab.id;
+//     chrome.runtime.reload();
 
 
-	// Send a message to the active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 	// Send a message to the active tab
+// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	
-	var activeTab = tabs[0];
-	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-	});
+// 	var activeTab = tabs[0];
+// 	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+// 	});
+// });
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({passive: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+
+
+chrome.tabs.getCurrent(function(tab) {
+    console.log(tab.title);
 });
 
 
-function reloadTab() {
-    var tabID = localStorage.tabToReload;
-    if (tabID) {
-        chrome.tabs.reload(parseInt(tabID));
-        delete(localStorage.tabToReload);
-    }
-}
-reloadTab();
+
--- extension/content.js
+++ extension/content.js
@@ -9,5 +9,77 @@
 //   }
 // )
 
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-alert(firstHref);
+// var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+// var bglog = function(obj) {
+// 	if(chrome && chrome.runtime) {
+// 		chrome.runtime.sendMessage({type: "bglog", obj: obj});
+// 	}
+// }
+
+// bglog(firstHref);
+
+//alert("hello world");
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "clicked_browser_action" ) {
+//       var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+//       console.log(firstHref);
+//     }
+//   }
+// );
+
+
+
+// var elements = document.getElementsByTagName("div");
+
+// var item1 = $('div._5rpb');
+// console.log (item1);
+// ==UserScript==
+// @name        SampleScript
+// @description Clear text of text inputs and write to console
+// @author      Matt
+// @include     *
+// @version     0.1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+
+
+
+setInterval(function(){ console.log ("timeout");}, 3000);
+
+var inputs = Array.prototype.slice.call(document.querySelectorAll('input, textarea, span'));
+
+inputs.forEach(function(i) {
+
+  	if (i.tagName.toLowerCase() == 'input' || i.tagName.toLowerCase() == 'textarea') {
+      i.addEventListener("input", function(e) {
+          e.preventDefault();
+          console.log(e.target.value);
+      });
+    } else {
+     	if (i.hasAttribute('data-text')) {
+        i.addEventListener("change", function(e) {
+          e.preventDefault();
+          console.log(e.target.value);
+      });
+      }
+    }
+});
+
+
+
+
+// for (var i = 0; i < elements.length; i++){
+// 	var element = elements[i]
+
+// 	console.log($("div"));
+// 	// for (var j = 0; j < element.childNodes.length; j++){
+// 	// 	var node = element.childNodes[j];
+// 	// 	console.log(node);
+// 	// }
+// }
