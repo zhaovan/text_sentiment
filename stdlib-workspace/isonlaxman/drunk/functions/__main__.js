0ea7d2fdca435b099018c79047b5ab5e0f78514e
Ivan Z <szi8890@users.noreply.github.com>
2018-12-01 02:06:44 -0500

    I tried team

--- stdlib-workspace/isonlaxman/drunk/functions/__main__.js
+++ stdlib-workspace/isonlaxman/drunk/functions/__main__.js
@@ -1,14 +1,27 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var spawn = require('child_process').spawn;
 
 /**
 * A basic Hello World function
-* @param {string} url 
+* @param {string} arg
 * @returns {object}
 */
 
-module.exports = async (url = 'world', context) => {
-  var xmlhttp;
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", url, true);
-  return xmlhttp;
+module.exports = async (arg = "John", context) => {
+  // var xmlhttp;
+  // xmlhttp = new XMLHttpRequest();
+  //
+  // // var params = JSON.stringify({}
+  // xmlhttp.open("POST", "test.py?word=" + arg, true);
+  // xmlhttp.responseType = "JSON";
+  // xmlhttp.onload = function(e) {
+  //   var arrOfStrings = JSON.parse(xmlhttp.response);
+  // }
+  // xmlhttp.send();
+  // return xmlhttp;
+
+  var pythonP = spawn('python', ['test.py']);
+  pythonP.stdout.on('data', (data) => {
+    return data;
+  })
 };
--- stdlib-workspace/isonlaxman/drunk/functions/test.py
+++ stdlib-workspace/isonlaxman/drunk/functions/test.py
@@ -2,8 +2,9 @@ import sys
 # print sys.argv[0] prints test.py
 # print sys.argv[1] prints your_var_1
 
-def hello():
-    print "Hi" + " " + sys.argv[1]
+def hello(word):
+    print ("Hi")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     hello()
