7739b3ac77f5577cf5f8df5b9b0f6d70fdb62c6f
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2018-12-01 20:27:27 -0500

    Minor change in flask test
    Allows you to pass in argument to the function

--- sentiment/test.py
+++ sentiment/test.py
@@ -4,9 +4,9 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/output', methods=['GET'])
-def output():
-	return "HELLO IT WORKS"
+@app.route('/output/<word>', methods=['GET'])
+def output(word):
+	return "HELLO " + word
 
 if __name__ == "__main__":
 	app.run()
