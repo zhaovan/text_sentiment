58f925d4b20336e96dcf921b4ae43ec9ffc21f93
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2018-12-01 19:59:57 -0500

    NLP
    Did the NLP thing, now setting up flask between

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,10 +1,19 @@
 from textblob import TextBlob
 from sys import argv
 
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/sentiment', methods=['GET'])
 def sentiment_analysis(inp):
     text = TextBlob(inp.readline())
     for sentence in text.sentences:
         print(sentence.sentiment)
+
 
 if __name__ == "__main__":
     f = open(argv[1], "r")
---
+++ trigger/trigger.py
@@ -0,0 +1,5 @@
+import json
+from sklearn.datasets import fetch_20newsgroups
+
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+
