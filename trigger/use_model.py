f310d4677eaffefbf5b3fe4637ad37df6887e30c
sonny3690 <sonny3690@gmail.com>
2018-12-01 20:58:43 -0500

    model can now be used

--- trigger/use_model.py
+++ trigger/use_model.py
@@ -1,12 +1,15 @@
 import pickle
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.datasets import fetch_20newsgroups
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import SGDClassifier
+# from sklearn.linear_model import SGDClassifier
 
+model
 
+def init ():
+	model = pickle.load(open( "trained_nlp.p", "rb" ))	
 
-model = pickle.load('trained_nlp.p',  open( "trained_nlp.p", "rb" ))
+def nlp_result (input):
+	return model.predict (input)
 
-print (type(model))
